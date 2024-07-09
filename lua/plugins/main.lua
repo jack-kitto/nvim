648d6c9759524fe26609c5548ba4e21a8508ede3
jack-kitto1 <jack.kitto@thenod.app>
2024-07-10 09:50:35 +1000

    override nvim-cmp and add cmp-emoji

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -199,4 +199,13 @@ return {
       -- refer to the configuration section below
     },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+    end,
+  },
 }
