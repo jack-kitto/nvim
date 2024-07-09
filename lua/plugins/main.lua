4fdd5aaf420e13e7b0f55ceeef66c695d329ada0
jack-kitto1 <jack.kitto@thenod.app>
2024-07-10 09:52:31 +1000

    add nx.nvim

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -219,4 +219,21 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     opts = {}, -- your configuration,
   },
+  {
+    "Equilibris/nx.nvim",
+
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+
+    opts = {
+      -- See below for config options
+      nx_cmd_root = "npx nx",
+    },
+
+    -- Plugin will load when you use these keys
+    keys = {
+      { "<leader>nx", "<cmd>Telescope nx actions<CR>", desc = "nx actions" },
+    },
+  },
 }
