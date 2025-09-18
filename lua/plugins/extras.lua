06396c29b16d45178a064175a542e4876b90a9c6
Jack Kitto <jack.kitto@gmail.com>
2025-09-19 07:54:19 +0900

    fix: remove mini starter

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -3,9 +3,6 @@ return {
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
   { import = "lazyvim.plugins.extras.lang.typescript" },
 
-  -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
-
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
 }
